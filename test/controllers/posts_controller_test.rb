d7c21477e8e3e26fb86b3a2732e93b5b8b09f5b6
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-02-24 09:57:52 +0200

    added pagination

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,10 +4,14 @@ require 'json'
 class PostsControllerTest < ActionController::TestCase
 
   test "Should get valid list of posts" do
-    get :index
+    get :index, params: { page: { number: 2 } }
     assert_response :success
     jdata = JSON.parse response.body
-    assert_equal Post.count, jdata['data'].length
+    assert_equal Post.per_page, jdata['data'].length
     assert_equal jdata['data'][0]['type'], 'posts'
+    l = jdata['links']
+    assert_equal l['first'], l['prev']
+    assert_equal l['last'], l['next']
+    assert_equal Post.count, jdata['meta']['total-count']
   end
 end
